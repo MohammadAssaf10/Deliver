b23a51e151908a72b7d52128fe932a4634427bd0
Mohammadassaf10 <mhamad234assaf@gmail.com>
2025-01-04 03:20:00 +0300

    home body UI

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ void main() async {
   // await Firebase.initializeApp(
   //   options: DefaultFirebaseOptions.currentPlatform,
   // );
+  //
   Bloc.observer = MyBlocObserver();
   runApp(
     BlocProvider<AppCubit>(
